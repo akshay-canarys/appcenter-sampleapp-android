47b70a764694349dba863338b686f3c156e27873
akshay-canarys <72204635+akshay-canarys@users.noreply.github.com>
2022-02-03 15:10:31 +0530

    added appp center secret

--- app/src/main/java/ms/appcenter/sampleapp/android/MainActivity.java
+++ app/src/main/java/ms/appcenter/sampleapp/android/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
                     Analytics.class, Crashes.class, Distribute.class);
         } else {
             // Otherwise use the hardcoded string value here
-            AppCenter.start(getApplication(), "<APP SECRET HERE>",
+            AppCenter.start(getApplication(), "d5ab856a-6ce8-466d-97fc-dff676be1a77",
                     Analytics.class, Crashes.class, Distribute.class);
         }
 
